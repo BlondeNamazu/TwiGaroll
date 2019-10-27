5adcc7bbbcc73e1c258c5628e6b7ca6c637ab99c
Namazu <namazu.titech15@gmail.com>
2019-10-28 03:48:50 +0900

    use external gitignored file to read CONSUMER_KEY and CONSUMER_API_KEY, and revoke them.

--- app/src/main/java/com/example/twigaroll/TwiGarollApplication.kt
+++ app/src/main/java/com/example/twigaroll/TwiGarollApplication.kt
@@ -9,15 +9,12 @@ import com.twitter.sdk.android.core.TwitterConfig
 
 class TwiGarollApplication : Application(){
 
-    private val CONSUMER_KEY = "7kk6FGu7YjK4oV5JPzAci9vzG"
-    private val CONSUMER_API_KEY = "KfY597zwd7vmCvzmb4mjykitCz0mGNAiCR3JfHxjiuG8KzpqYU"
-
     override fun onCreate() {
         super.onCreate()
 
         val config = TwitterConfig.Builder(this)
             .logger(DefaultLogger(Log.DEBUG))
-            .twitterAuthConfig(TwitterAuthConfig(CONSUMER_KEY,CONSUMER_API_KEY))
+            .twitterAuthConfig(TwitterAuthConfig(getString(R.string.CONSUMER_KEY),getString(R.string.CONSUMER_API_KEY)))
             .debug(true)
             .build()
         Twitter.initialize(config)
