387d5f3d64a604993e73d4a80bfceb663b838d96
BlondeNamazu <namazutitech15@gmail.com>
2020-01-27 12:39:19 +0900

    :wip:

--- app/src/main/java/com/example/twigaroll/home/gallery/GalleryViewModel.kt
+++ app/src/main/java/com/example/twigaroll/home/gallery/GalleryViewModel.kt
@@ -8,43 +8,39 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.twigaroll.home.gallery_detail.GalleryDetailFragment
 import com.example.twigaroll.R
+import com.example.twigaroll.data.TweetIdData
+import com.example.twigaroll.util.FileIORepository
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.KotlinJsonAdapterFactory
+import com.squareup.moshi.Moshi
 import com.twitter.sdk.android.core.Callback
 import com.twitter.sdk.android.core.Result
 import com.twitter.sdk.android.core.TwitterCore
 import com.twitter.sdk.android.core.TwitterException
 import com.twitter.sdk.android.core.models.Tweet
+import dagger.Component
+import javax.inject.Inject
 
-class GalleryViewModel : ViewModel() {
+@Component
+class GalleryViewModel @Inject constructor(
+    private val fileIORepository: FileIORepository
+) : ViewModel() {
 
     private val _imageURLs = MutableLiveData<List<String>>()
     val imageURLs: LiveData<List<String>>
         get() = _imageURLs
     private lateinit var navigator: GalleryNavigator
+    val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+    private val converter: JsonAdapter<TweetIdData> = moshi.adapter(TweetIdData::class.java)
 
     fun refreshTimeline(v: View) {
-        val twitterApiClient = TwitterCore.getInstance().apiClient
-        val statusesService = twitterApiClient.statusesService
-
-        val call = statusesService.homeTimeline(200, null, null, null, null, null, null)
-
-        call.enqueue(object : Callback<List<Tweet>>() {
-            override fun success(result: Result<List<Tweet>>?) {
-                result ?: return
-                _imageURLs.value = result.data.filter {
-                    it.extendedEntities.media.isNotEmpty()
-                }.flatMap {
-                    it.extendedEntities.media
-                }.map {
-                    it.mediaUrlHttps
-                }
-                Log.d("Namazu", "success to get timeline")
-            }
-
-            override fun failure(exception: TwitterException?) {
-                Log.d("Namazu", "failed to get timeline : ${exception.toString()}")
-            }
-        })
-
+        val json = fileIORepository.readFile(v.context)
+        val data = if (json.isEmpty()) {
+            TweetIdData(emptyArray())
+        } else {
+            converter.fromJson(json) ?: TweetIdData(emptyArray())
+        }
+        _imageURLs.value = data.mediaURLs.toList()
     }
 
     fun setNavigator(navigator: GalleryNavigator) {
