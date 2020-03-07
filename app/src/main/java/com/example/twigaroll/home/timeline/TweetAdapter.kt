7a6115c25e19861f10942d58d2163515894b7c5f
BlondeNamazu <namazutitech15@gmail.com>
2020-03-08 01:20:44 +0900

    implement retweet / unretweet

--- app/src/main/java/com/example/twigaroll/home/timeline/TweetAdapter.kt
+++ app/src/main/java/com/example/twigaroll/home/timeline/TweetAdapter.kt
@@ -50,8 +50,12 @@ class TweetAdapter @Inject constructor(
             convertView.tag as TweetRowBinding
         }.apply {
             tweet = tweetList[position]
-            favCount = tweetList[position].favoriteCount - if(tweetList[position].favorited) 1 else  0
+            favCount =
+                tweetList[position].favoriteCount - if (tweetList[position].favorited) 1 else 0
             favorited = tweetList[position].favorited
+            retCount =
+                tweetList[position].retweetCount - if (tweetList[position].retweeted) 1 else 0
+            retweeted = tweetList[position].retweeted
             stockButton.setOnClickListener {
                 val json = fileIORepository.readFile(parent.context)
                 val data = if (json.isEmpty()) {
@@ -79,38 +83,92 @@ class TweetAdapter @Inject constructor(
                 val twitterApiClient = TwitterCore.getInstance().apiClient
                 val statusesService = twitterApiClient.favoriteService
 
-                if(favorited){
-                    val call = statusesService.destroy(tweetList[position].id,false)
+                if (favorited) {
+                    val call = statusesService.destroy(tweetList[position].id, false)
 
-                    call.enqueue(object: Callback<Tweet>(){
+                    call.enqueue(object : Callback<Tweet>() {
                         override fun success(result: Result<Tweet>?) {
                             Log.d("Namazu", "success to unfav! contents:${result?.data?.text}")
-                            Toast.makeText(parent.context,"Unfav succeed!",Toast.LENGTH_LONG).show()
+                            Toast.makeText(parent.context, "Unfav succeed!", Toast.LENGTH_LONG)
+                                .show()
                             favorited = false
                         }
 
                         override fun failure(exception: TwitterException?) {
                             Log.d("Namazu", "failed to unfav...")
-                            Toast.makeText(parent.context,"Unfav failed: ${exception?.message}",Toast.LENGTH_LONG).show()
+                            Toast.makeText(
+                                parent.context,
+                                "Unfav failed: ${exception?.message}",
+                                Toast.LENGTH_LONG
+                            ).show()
                         }
                     })
                 } else {
-                    val call = statusesService.create(tweetList[position].id,false)
+                    val call = statusesService.create(tweetList[position].id, false)
 
-                    call.enqueue(object: Callback<Tweet>(){
+                    call.enqueue(object : Callback<Tweet>() {
                         override fun success(result: Result<Tweet>?) {
                             Log.d("Namazu", "success to fav! contents:${result?.data?.text}")
-                            Toast.makeText(parent.context,"Fav succeed!",Toast.LENGTH_LONG).show()
+                            Toast.makeText(parent.context, "Fav succeed!", Toast.LENGTH_LONG).show()
                             favorited = true
                         }
 
                         override fun failure(exception: TwitterException?) {
                             Log.d("Namazu", "failed to fav...")
-                            Toast.makeText(parent.context,"Fav failed: ${exception?.message}",Toast.LENGTH_LONG).show()
+                            Toast.makeText(
+                                parent.context,
+                                "Fav failed: ${exception?.message}",
+                                Toast.LENGTH_LONG
+                            ).show()
                         }
                     })
                 }
 
+            }
+            retweetButton.setOnClickListener {
+                val twitterApiClient = TwitterCore.getInstance().apiClient
+                val statusesService = twitterApiClient.statusesService
+
+                if (retweeted) {
+                    val call = statusesService.unretweet(tweetList[position].id, false)
+
+                    call.enqueue(object : Callback<Tweet>() {
+                        override fun success(result: Result<Tweet>?) {
+                            Log.d("Namazu", "success to unretweet! contents:${result?.data?.text}")
+                            Toast.makeText(parent.context, "Unretweet succeed!", Toast.LENGTH_LONG)
+                                .show()
+                            retweeted = false
+                        }
+
+                        override fun failure(exception: TwitterException?) {
+                            Log.d("Namazu", "failed to unretweet...")
+                            Toast.makeText(
+                                parent.context,
+                                "Unretweet failed: ${exception?.message}",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+                    })
+                } else {
+                    val call = statusesService.retweet(tweetList[position].id,false)
+
+                    call.enqueue(object : Callback<Tweet>() {
+                        override fun success(result: Result<Tweet>?) {
+                            Log.d("Namazu", "success to ret! contents:${result?.data?.text}")
+                            Toast.makeText(parent.context, "Retweet succeed!", Toast.LENGTH_LONG).show()
+                            retweeted = true
+                        }
+
+                        override fun failure(exception: TwitterException?) {
+                            Log.d("Namazu", "failed to retweet...")
+                            Toast.makeText(
+                                parent.context,
+                                "Retweet failed: ${exception?.message}",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+                    })
+                }
             }
         }
         return binding.root
