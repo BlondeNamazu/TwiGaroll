d4f894d6cda6cafac9b208f9a06a729999e9a4d1
BlondeNamazu <namazutitech15@gmail.com>
2020-03-25 13:24:36 +0900

    show stamp candidates under baseTweet

---
+++ app/src/main/java/com/example/twigaroll/home/timeline/StampListAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.twigaroll.home.timeline
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.recyclerview.widget.RecyclerView
+import com.example.twigaroll.R
+import com.example.twigaroll.data.TweetIdData
+import com.example.twigaroll.databinding.ReplyStampListItemBinding
+import com.example.twigaroll.util.BindingViewHolder
+import com.example.twigaroll.util.FileIORepository
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.KotlinJsonAdapterFactory
+import com.squareup.moshi.Moshi
+import javax.inject.Inject
+
+class StampListAdapter @Inject constructor(
+    private val fileIORepository: FileIORepository
+) :
+    RecyclerView.Adapter<BindingViewHolder>() {
+
+    private val _imageURLs = MutableLiveData<List<String>>()
+    val imageURLs: LiveData<List<String>>
+        get() = _imageURLs
+    val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+    private val converter: JsonAdapter<TweetIdData> = moshi.adapter(TweetIdData::class.java)
+
+    fun loadStamps(v: View) {
+        val json = fileIORepository.readFile(v.context)
+        val data = if (json.isEmpty()) {
+            TweetIdData(emptyArray())
+        } else {
+            converter.fromJson(json) ?: TweetIdData(emptyArray())
+        }
+        _imageURLs.value = data.mediaURLs.toList()
+    }
+
+    override fun getItemCount(): Int = imageURLs.value?.size ?: 0
+
+    override fun onBindViewHolder(holder: BindingViewHolder, position: Int) {
+        (holder.binding as ReplyStampListItemBinding).imageURL =
+            imageURLs.value?.get(position)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        BindingViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.reply_stamp_list_item, parent, false)
+        )
+}
